bee3534417fe86e15627edb6bbded77fb15b6c6a
PavelRPavlov <pavel.raykov.pavlov@gmail.com>
2015-06-10 17:13:45 +0300

    try to fix the build

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 bundle install --without development || { echo 'Installing Ruby gems failed.'; exit 1; }
 
 # Build the documentation
-jekyll build || { echo 'Building the documentation with Jekyll failed.'; exit 1; }
+ bundle exec jekyll build || { echo 'Building the documentation with Jekyll failed.'; exit 1; }
 
 # Copy the _site directory to \\telerik.com\Resources\AppDirector\Drops\Xamarin\Xamarin.Docs
 robocopy _site //telerik.com/Resources/AppDirector/Drops/Xamarin/Xamarin.Docs -purge -s

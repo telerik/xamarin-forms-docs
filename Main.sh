ad87d1ea19dbd0e05861a89184760af914df605a
Tsvyatko Konov <tsvyatco@gmailcom>
2016-09-05 15:55:36 +0300

    updated main.sh to test only building of snippets

--- Main.sh
+++ Main.sh
@@ -1,13 +1,9 @@
 
 cp -r ../Source Output
 
-REM rd /s /q markdown-snippet-injector
-REM   git clone https://github.com/NativeScript/markdown-snippet-injector.git
-
 cd ../markdown-snippet-injector
 call npm install
 
 node . --root=../Xamarin-Forms/Examples/XamarinForms --docsroot=Output --sourceext=".cs|.xaml" --snippettitles="C#|XAML"
 
 cd ../Xamarin-Forms-docs
-bash deploy.sh

93de73b37e193a755d4dd5918e064e1c7ecf4fdd
Tsvyatko Konov <tsvyatco@gmailcom>
2016-09-05 16:14:48 +0300

    update selected

--- Main.sh
+++ Main.sh
@@ -1,8 +1,8 @@
 
 
-cd ../markdown-snippet-injector
+cd ${WORKSPACE}/markdown-snippet-injector
 call npm install
 
 node . --root=../Xamarin-Forms/Examples/XamarinForms --docsroot=../Xamarin-docs/Source --sourceext=".cs|.xaml" --snippettitles="C#|XAML"
 
-cd ../Xamarin-Forms-docs
+cd ${WORKSPACE}/Xamarin-Forms-docs

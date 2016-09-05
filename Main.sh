7c9e7940b58a985a1733cd9db19c5e0794509274
Tsvyatko Konov <tsvyatco@gmailcom>
2016-09-05 16:24:36 +0300

    updated folder structure

--- Main.sh
+++ Main.sh
@@ -3,6 +3,6 @@
 cd ${WORKSPACE}/markdown-snippet-injector
 npm install
 
-node . --root=../Xamarin-Forms/Examples/XamarinForms --docsroot=../Xamarin-docs/Source --sourceext=".cs|.xaml" --snippettitles="C#|XAML"
+node . --root=../Xamarin-Forms/Examples/XamarinForms --docsroot=../Xamarin-Forms-docs --sourceext=".cs|.xaml" --snippettitles="C#|XAML"
 
 cd ${WORKSPACE}/Xamarin-Forms-docs
